7b5b6142c882ae2be4875b7717a81256b19b1103
Firoz-Alam <51852342+Firoz-Alam@users.noreply.github.com>
2022-03-29 13:21:13 +0600

    second commit

---
+++ prototype.js
@@ -0,0 +1,21 @@
+function Person(name,age){
+
+    let person = {};
+
+    person.name = name;
+    person.age = age;
+
+    person.eat = function(){
+        console.log('person is eating');
+    }
+
+    person.sleep = function(){
+        console.log('person is sleeping');
+    }
+return person;
+
+}
+
+const sakib = Person('Sakib',35);
+sakib.eat();
+const tamim = Person('Tamim',36);
