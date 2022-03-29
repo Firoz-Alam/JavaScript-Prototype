0b1b88035c5fd5941c80946cd6592e7590318abe
Firoz-Alam <51852342+Firoz-Alam@users.noreply.github.com>
2022-03-29 15:18:29 +0600

    class converte

--- prototype.js
+++ prototype.js
@@ -1,28 +1,47 @@
 
 
-function Person(name,age){
+// function Person(name,age){
 
-    // let person = Object.create(Person.prototype);  //every function has prototype property
+//     // let person = Object.create(Person.prototype);  //every function has prototype property
 
-    this.name = name;
-    this.age = age;
+//     this.name = name; 
+//     this.age = age;
 
   
-// return person;
+// // return person;
 
-}
+// }
 
-Person.prototype = {
-    eat(){
-        console.log('person is eating');
-    },
-    sleep(){
-        console.log('person is sleeping');
+// Person.prototype = {
+//     eat(){
+//         console.log('person is eating');
+//     },
+//     sleep(){
+//         console.log('person is sleeping');
+//     }
+// }
+
+// const sakib =new Person('Sakib',35); // new keyword added and create new object 
+// sakib.eat();
+// const tamim = new Person('Tamim',36);
+
+
+// class converted
+
+class Person{
+    constructor(name,age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    eat() {
+        console.log('Person is eating');
+    }
+    sleep() {
+        console.log('Person is sleeping');
     }
 }
 
-const sakib =new Person('Sakib',35); // new keyword added and create new object 
+const sakib = new Person("Sakib",35);
 sakib.eat();
-const tamim = new Person('Tamim',36);
-
 
